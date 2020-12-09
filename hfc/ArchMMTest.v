9ac3039b6c3532b643e8029e77bf6b5ebdae4f95
Jieung Kim <je1004k@gmail.com>
2020-12-09 18:44:26 +0900

    add test file

--- hfc/ArchMMTest.v
+++ hfc/ArchMMTest.v
@@ -1,3 +1,18 @@
+(*
+ * Copyright 2020 Youngju Song / Jieung Kim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *)
 From Coq Require Import
      Arith.PeanoNat
      Lists.List
@@ -38,8 +53,24 @@ Require Import Lang.
 Require Import Values.
 Require Import Integers.
 Require Import Constant.
+Require Import ArchMM.
+Require Import ArchMMHighSpec.
 Import LangNotations.
 Local Open Scope expr_scope.
 Local Open Scope stmt_scope.
 
+Import ArchMM.
 Import Int64.
+
+Set Implicit Arguments.
+
+
+Module ArchMMTEST.
+
+(* some unit tests *)
+
+
+
+End ArchMMTEST.
+
+  
